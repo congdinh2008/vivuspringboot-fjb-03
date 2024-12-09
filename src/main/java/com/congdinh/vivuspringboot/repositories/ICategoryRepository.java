7cb7a4708cde8974f8d91d844d9d625e7a674892
Cong Dinh <congdinh2012@hotmail.com>
2024-12-09 15:03:33 +0700

    Step 4: Create Category Entity and Repository

---
+++ src/main/java/com/congdinh/vivuspringboot/repositories/ICategoryRepository.java
@@ -0,0 +1,11 @@
+package com.congdinh.vivuspringboot.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.congdinh.vivuspringboot.entities.Category;
+
+public interface ICategoryRepository extends JpaRepository<Category, UUID> {
+
+}
